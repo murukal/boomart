ab4060b08172d018c6f65e985dff9bc0eaf0122c
DreamCalledDrew <tutu@fantufantu.com>
2022-06-20 11:26:53 +0800

    feat: 添加隐私政策页面

--- next.config.js
+++ next.config.js
@@ -2,18 +2,31 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-/** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-
-  images: {
-    domains: ['boomemory-1304340057.cos.ap-shanghai.myqcloud.com']
-  },
-
-  /** webpack */
-  webpack: (config) => {
-    // 设置文件路径别名
-    config.resolve.alias['~'] = __dirname
-    return config
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: []
   }
 })
+
+/** @type {import('next').NextConfig} */
+module.exports = withMDX(
+  withBundleAnalyzer({
+    reactStrictMode: true,
+
+    images: {
+      domains: ['boomemory-1304340057.cos.ap-shanghai.myqcloud.com']
+    },
+
+    /** webpack */
+    webpack: (config) => {
+      // 设置文件路径别名
+      config.resolve.alias['~'] = __dirname
+      return config
+    },
+
+    // Append the default value with md extensions
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+  })
+)
